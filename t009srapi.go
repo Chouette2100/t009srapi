29a561981dc71522ebc1c43309b99d869f068ebe
Chouette2100 <chouette.21.00@gmail.com>
2022-08-27 22:01:03 +0900

    c0.2.0 excl.txtとrvl.txtの追加にともなるリリースの修正にともなってコメントを変更した。

--- t009srapi.go
+++ t009srapi.go
@@ -41,9 +41,9 @@ import (
 【Unix/Linux】
 
 	$ cd ~/go/src
-	$ curl -OL https://github.com/Chouette2100/t009srapi/archive/refs/tags/v0.1.0.tar.gz
-	$ tar xvf v0.1.0.tar.gz
-	$ mv t009srapi-0.1.0 t009srapi
+	$ curl -OL https://github.com/Chouette2100/t009srapi/archive/refs/tags/v0.2.0.tar.gz
+	$ tar xvf v0.2.0.tar.gz
+	$ mv t009srapi-0.2.0 t009srapi
 	$ cd t009srapi
 	$ go mod init
 	$ go mod tidy
@@ -65,42 +65,43 @@ import (
 
 	作業はかならず %HOMEPATH%\go\src の下で行います。
 
-	以下、要するに https://github.com/Chouette2100/t009srapi/releases にあるv0.1.0のZIPファイルSource code (zip) からソースをとりだしてくださいということなので、ブラウザでダウンロードしてエクスプローラで解凍というこでもけっこうです。なんならこの記事の最後にあるgithubのソースをエディターにコピペで作るということでもかまいません（この場合文字コードはかならずUTF-8にしてください 改行はLFになっています。というようなことを考えるとやっぱりダウンロードして解凍が安全かも）
+	以下、要するに https://github.com/Chouette2100/t009srapi/releases にあるv0.2.0のZIPファイルSource code (zip) からソースをとりだしてくださいということなので、ブラウザでダウンロードしてエクスプローラで解凍というこでもけっこうです。なんならこの記事の最後にあるgithubのソースをエディターにコピペで作るということでもかまいません（この場合文字コードはかならずUTF-8にしてください 改行はLFになっています。というようなことを考えるとやっぱりダウンロードして解凍が安全かも）
 
 	C:\Users\chouette\go\src>mkdir t009srapi
 
 	C:\Users\chouette\go\src>cd t009srapi
 
-	C:\Users\chouette\go\src\t009srapi>curl -OL https://github.com/Chouette2100/t009srapi/archive/refs/tags/v0.1.0.zip
+	C:\Users\chouette\go\src\t009srapi>curl -OL https://github.com/Chouette2100/t009srapi/archive/refs/tags/v0.2.0.zip
 	  % Total    % Received % Xferd  Average Speed   Time    Time     Time  Current
 	                                 Dload  Upload   Total   Spent    Left  Speed
 	  0     0    0     0    0     0      0      0 --:--:-- --:--:-- --:--:--     0
 	100  6265    0  6265    0     0   6777      0 --:--:-- --:--:-- --:--:-- 16400
 
-	C:\Users\chouette\go\src\t009srapi>call powershell -command "Expand-Archive v0.1.0.zip"
+	C:\Users\chouette\go\src\t009srapi>call powershell -command "Expand-Archive v0.2.0.zip"
 
 	C:\Users\chouette\go\src\t009srapi>tree
 	フォルダー パスの一覧
 	ボリューム シリアル番号は E2CD-BDF1 です
 	C:.
-	└─v0.1.0
-	    └─t009srapi-0.1.0
+	└─v0.2.0
+	    └─t009srapi-0.2.0
 	        ├─public
 	        └─templates
 
-	C:\Users\chouette\go\src\t009srapi>xcopy /e v0.1.0\t009srapi-0.1.0\*.*
-	v0.1.0\t009srapi-0.1.0\freebsd.bat
-	v0.1.0\t009srapi-0.1.0\freebsd.sh
-	v0.1.0\t009srapi-0.1.0\LICENSE
-	v0.1.0\t009srapi-0.1.0\README.md
-	v0.1.0\t009srapi-0.1.0\t009srapi.go
-	v0.1.0\t009srapi-0.1.0\public\index.html
-	v0.1.0\t009srapi-0.1.0\templates\top.gtpl
-	7 File(s) copied
+	C:\Users\chouette\go\src\t009srapi>xcopy /e v0.2.0\t009srapi-0.2.0\*.*
+	v0.2.0\t009srapi-0.2.0\freebsd.bat
+	v0.2.0\t009srapi-0.2.0\freebsd.sh
+	v0.2.0\t009srapi-0.2.0\LICENSE
+	v0.2.0\t009srapi-0.2.0\README.md
+	v0.2.0\t009srapi-0.2.0\t009srapi.go
+	v0.2.0\t009srapi-0.2.0\public\index.html
+	v0.2.0\t009srapi-0.2.0\templates\top.gtpl
+	以下省略（リリースによって内容が異なる場合があります。）
 
-	C:\Users\chouette\go\src\t009srapi>rmdir /s /q v0.1.0
+	C:\Users\chouette\go\src\t009srapi>rmdir /s /q v0.2.0
+	以下省略（リリースによって内容が異なる場合があります。）
 
-	C:\Users\chouette\go\src\t009srapi>del v0.1.0.zip
+	C:\Users\chouette\go\src\t009srapi>del v0.2.0.zip
 
 	ここで次のような構成になっていればOKです。top.gtpl と index.html が所定の場所にあることをかならず確かめてください。
 
@@ -136,7 +137,7 @@ import (
 
 	で、実行時点でのイベントの一覧が表示されます。
 
-	Ver. 0.1.0
+	Ver. 0.2.0
 
 */
 
