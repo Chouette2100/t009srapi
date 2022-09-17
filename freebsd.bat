6fd5c2cb7a3ff1637be813f29ebb5e13679fd939
Chouette2100 <chouette.21.00@gmail.com>
2022-09-18 07:56:26 +0900

    クロスコンパイルのシェルを一つにまとめる

--- freebsd.bat
+++
@@ -1,9 +0,0 @@
-set GOOS_TMP=%GOOS%
-set GOARCH_TMP=%GOARCH%
-set GOOS=freebsd
-set GOARCH=amd64
-go build -o t009srapi.freebsd t009srapi.go
-set GOOS=%GOOS_TMP%
-set GOARCH=%GOARCH_TMP%
-set GOOS_TMP=
-set GOARCH_TMP=
--- linux.bat
+++
@@ -1,9 +0,0 @@
-set GOOS_TMP=%GOOS%
-set GOARCH_TMP=%GOARCH%
-set GOOS=linux
-set GOARCH=amd64
-go build -o t009srapi.linux t009srapi.go
-set GOOS=%GOOS_TMP%
-set GOARCH=%GOARCH_TMP%
-set GOOS_TMP=
-set GOARCH_TMP=
